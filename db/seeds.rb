ce015e3f659b13c97f910fec8ea87c06122eb971
Vanecia Thompson <vaneciaathompson@gmail.com>
2017-05-15 20:35:50 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = Category.create([{name: 'Computers'}, {name: 'Televisions'}, {name: 'Smart Phones'}, {name: 'Stereos'}, {name: 'Speakers'}, {name: 'Others'}])
+#categories = Category.create([{name: 'Computers'}, {name: 'Televisions'}, {name: 'Smart Phones'}, 
+	#{name: 'Stereos'}, {name: 'Speakers'}, {name: 'Others'}])
 
-products = Product.create([{name: "MackBook Pro", price: 1299.00, quantity: 30, brand: "Apple"}, {name: "G3", price: 430.79, quantity: 40, brand: "LG"}, {name: "LED TV", price: 600.88, quantity: 16, brand: "Sony"}])
+#products = Product.create([{name: "MackBook Pro", price: 1299.00, quantity: 30, brand: "Apple"}, 
+	#name: "G3", price: 430.79, quantity: 40, brand: "LG"}, {name: "LED TV", price: 600.88, quantity: 16, brand: "Sony"}])
