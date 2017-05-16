a574497a059fa554ef8dcb043b2fba7a178206d3
Vanecia Thompson <vaneciaathompson@gmail.com>
2017-05-15 20:39:45 -0400

    add storefront controller + views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'storefront/all_items'
+
+  get 'storefront/items_by_category'
+
+  get 'storefront/items_by_brand'
+
+  root 'storefront#all_items'
+ 
+  get 'categorical' => 'storefront#items_by_category' 
+  get 'branding' => 'storefront#items_by_brand'
+
   resources :orders
   resources :line_items
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
